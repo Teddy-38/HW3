d9cd5b50db9cd687672e5457ed7d37090f30d606
Teddy-38 <Zhdanov_as94@mail.ru>
2024-12-31 00:11:11 +0800

    1 mission

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+        int a;
+        a = 100000;
+        byte b;
+        b = 100;
+        short c;
+        c = 10000;
+        long d;
+        d = 320000000;
+        float g;
+        g = 3.2f;
+        double q;
+        q = 1.7e+31;
+         System.out.println( " значение переменной 'a' с типом int равно " + a );
+        System.out.println( " значение переменной 'b' с типом byte равно " + b );
+        System.out.println( " значение переменной 'c' с типом short равно " + c);
+        System.out.println( " значение переменной 'd' с типом long равно " + d );
+        System.out.println( " значение переменной 'g' с типом float равно " + g );
+        System.out.println( " значение переменной 'q' с типом double равно " + q );}
+
 }
